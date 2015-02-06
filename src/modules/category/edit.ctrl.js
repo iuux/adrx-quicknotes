1da1863002384969f342072ebf6da8780880dfb9
Chris Basham <chris@bash.am>
2015-02-06 15:56:38 -0500

    Add cancel functionality to edit views

--- src/modules/category/edit.ctrl.js
+++ src/modules/category/edit.ctrl.js
@@ -1,7 +1,11 @@
 'use strict';
 
-module.exports = function($stateParams, categoryService) {
+module.exports = function($stateParams, $state, categoryService) {
   var id = parseInt($stateParams.id);
   var category = categoryService.get(id)
   this.category = angular.copy(category);
+
+  this.cancel = function() {
+    $state.transitionTo('app');
+  };
 };
--- src/modules/note/edit.ctrl.js
+++ src/modules/note/edit.ctrl.js
@@ -1,7 +1,11 @@
 'use strict';
 
-module.exports = function($stateParams, noteService) {
+module.exports = function($stateParams, $state, noteService) {
   var id = parseInt($stateParams.id);
   var note = noteService.get(id)
   this.note = angular.copy(note);
+
+  this.cancel = function() {
+    $state.transitionTo('app');
+  };
 };
