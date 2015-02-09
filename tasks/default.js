a63e8590e31c80f5944f7b375a318341ad2b2bf4
Chris Basham <chris@bash.am>
2015-02-09 10:23:27 -0500

    Start scripts and styles tasks before browser-sync task

--- tasks/default.js
+++ tasks/default.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('default', function(callback) {
-  runSequence('clean', ['build', 'vendor'], 'browser-sync', ['styles', 'scripts'], 'watch', callback);
+  runSequence('clean', ['build', 'styles', 'vendor', 'scripts'], 'browser-sync', 'watch', callback);
 });
