bd9d6f2ba8f80d7eddefa6c146889453a715fcf6
Chris Basham <chris@bash.am>
2015-02-13 14:28:36 -0500

    Correct task reference

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 gulp.task('watch', function() {
-  gulp.watch('./src/*.html', ['build', browserSync.reload]);
+  gulp.watch('./src/*.html', ['html', browserSync.reload]);
   gulp.watch('./src/**/*.less', ['styles']);
   gulp.watch('./src/**/*.{js,json,jsx}', ['scripts', browserSync.reload]);
 });
