2ec4da38b542a90d22463147fc231d5bf9f58d67
Chris Basham <chris@bash.am>
2015-02-20 16:02:06 -0500

    Integrate rev-del plugin into rev task

--- tasks/rev.js
+++ tasks/rev.js
@@ -1,19 +1,16 @@
 var gulp = require('gulp');
-var del = require('del');
 var rev = require('gulp-rev');
+var revDel = require('rev-del');
 
-// Would like to replace this with `rev-del`, but not getting it to work.
-// https://github.com/callumacrae/rev-del
-gulp.task('rev-del', function(callback) {
-  del(['./build/**/{styles-*.css,scripts-*.js,vendor-*.js}'], callback);
-});
-
-gulp.task('rev', ['rev-del'], function() {
+gulp.task('rev', function() {
   return gulp.src([
       './build/**/{styles.css,scripts.js,vendor.js}'
     ])
     .pipe(rev())
     .pipe(gulp.dest('./build'))
     .pipe(rev.manifest())
+    .pipe(revDel({
+      dest: './build'
+    }))
     .pipe(gulp.dest('./build'));
 });
