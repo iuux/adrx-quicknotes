d03d724552f44e9e480c823263c1f97838bf411a
Chris Basham <chris@bash.am>
2015-02-23 11:55:29 -0500

    Add function to parse query parameters

--- src/scripts/stores/mixins.js
+++ src/scripts/stores/mixins.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports = {
+  // http://codereview.stackexchange.com/a/10396
+  getQueryParams: function() {
+    var query = (window.location.search || '?').substr(1);
+    var map = {};
+
+    query.replace(/([^&=]+)=?([^&]*)(?:&+|$)/g, function(match, key, value) {
+      map[decodeURIComponent(key)] = decodeURIComponent(value);
+    });
+
+    return map;
+  },
   // http://stackoverflow.com/a/105074
   guid: function() {
     function s4() {
